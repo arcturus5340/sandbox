40762e545f95370eb434e9cd0fbb644dd90a16a9
arcturus5340 <arcturus5340@gmail.com>
2020-04-11 20:28:24 +0200

    Added module for basic generator creation

---
+++ generators.pas
@@ -0,0 +1,34 @@
+﻿unit generators;
+
+interface 
+
+function generator(arr: array of integer): function(): integer;
+
+
+implementation
+
+type
+  __Generator_class = class
+    arr: array of integer;
+    index: integer;
+    
+    function next(): integer;
+    begin
+      result := arr[index];
+      index += 1;
+      if index = arr.Length then
+        index := 0;
+    end;
+  
+  end;
+  
+function generator(arr: array of integer): function(): integer;
+begin
+  var obj := new __Generator_class;
+  obj.arr := arr;
+  obj.index := 0;
+  Result := obj.next;
+end;
+
+begin
+end.
